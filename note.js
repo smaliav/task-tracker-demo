80c146e2ec9dfc289feb4fe4fa21ca91989b8b66
Andrey Smaliy <32042194+smaliav@users.noreply.github.com>
2020-06-04 15:09:56 +0200

    Added demo version

---
+++ note.js
@@ -0,0 +1,88 @@
+const Note = {
+    idCounter: 8,
+    dragged: null,
+
+    process(noteElement) {
+        noteElement.addEventListener('dblclick', (e) => {
+            noteElement.removeAttribute('draggable');
+            noteElement.closest('.column').removeAttribute('draggable');
+            noteElement.setAttribute('contenteditable', 'true');
+            noteElement.focus()
+        })
+        
+        noteElement.addEventListener('blur', (e) => {
+            noteElement.setAttribute('draggable', 'true');
+            noteElement.closest('.column').setAttribute('draggable', 'true');
+            noteElement.removeAttribute('contenteditable');
+    
+            if (!noteElement.textContent.trim().length) {
+                noteElement.remove();
+            }
+        })
+    
+        noteElement.addEventListener('dragstart', Note.dragstart);
+        noteElement.addEventListener('dragend', Note.dragend);
+        noteElement.addEventListener('dragenter', Note.dragenter);
+        noteElement.addEventListener('dragover', Note.dragover);
+        noteElement.addEventListener('dragleave', Note.dragleave);
+        noteElement.addEventListener('drop', Note.drop);
+    },
+
+    dragstart(e) {
+        Node.dragged = this;
+        this.classList.add('dragged');
+        
+        e.stopPropagation();
+    },
+    
+    dragend(e) {
+        Node.dragged = null;
+        this.classList.remove('dragged');
+    
+        document.querySelectorAll('.note').forEach(x => x.classList.remove('under'));
+    },
+    
+    dragenter(e) {
+        if (this === Node.dragged) {
+            return;
+        }
+    
+        this.classList.add('under');
+    },
+    
+    dragover(e) {
+        e.preventDefault();
+    
+        if (this === Node.dragged) {
+            return;
+        }        
+    },
+    
+    dragleave(e) {
+        if (this === Node.dragged) {
+            return;
+        }
+        
+        this.classList.remove('under');
+    },
+    
+    drop(e) {
+        e.stopPropagation();
+    
+        if (this === Node.dragged) {
+            return;
+        }
+        
+        if (this.parentElement === Node.dragged.parentElement) {
+            const notes = Array.from(this.parentElement.querySelectorAll('.note'));
+            const indexDragged = notes.indexOf(Node.dragged);
+            const indexUnder = notes.indexOf(this);
+    
+            if (indexUnder > indexDragged) {
+                this.parentElement.insertBefore(Node.dragged, this.nextElementSibling);
+            } else {
+                this.parentElement.insertBefore(Node.dragged, this);
+            }
+        }
+    }
+}
